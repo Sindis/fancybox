dfa1ecc2fb9a7c34cee14f67222028896a626d05
Sebastian Domagala <sebdom93@gmail.com>
2015-01-20 22:38:01 +0100

    added-images

--- package.js
+++ package.js
@@ -7,6 +7,13 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
+  api.addFiles('./fancyBox/blank.gif','client');
+  api.addFiles('./fancyBox/fancybox_loading.gif','client');
+  api.addFiles('./fancyBox/fancybox_loading@2x.gif','client');
+  api.addFiles('./fancyBox/fancybox_overlay.png','client');
+  api.addFiles('./fancyBox/fancybox_sprite.png','client');
+  api.addFiles('./fancyBox/fancybox_sprite@2x.png','client');
+  api.addFiles('./fancyBox/helpers/fancybox_buttons.png','client');
   api.addFiles('./fancyBox/jquery.fancybox.css','client');
   api.addFiles('./fancyBox/helpers/jquery.fancybox-buttons.css','client');
   api.addFiles('./fancyBox/helpers/jquery.fancybox-thumbs.css','client');
@@ -14,6 +21,7 @@ Package.onUse(function(api) {
   api.addFiles('./fancyBox/helpers/jquery.fancybox-buttons.js','client');
   api.addFiles('./fancyBox/helpers/jquery.fancybox-media.js','client');
   api.addFiles('./fancyBox/helpers/jquery.fancybox-thumbs.js','client');
+
 });
 
 Package.onTest(function(api) {
